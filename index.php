7e4f3f1377ff23b6b16651a8876c0fc6196576ca
sarolong <sergiorolong98@gmail.com>
2022-06-23 15:31:56 -0500

    V3.0

--- index.php
+++ index.php
@@ -139,7 +139,7 @@
             <li class="nav-item">
                 <a class="nav-link" href="tables.html">
                     <i class="fas fa-fw fa-table"></i>
-                    <span>Tables</span></a>
+                    <span>Tablas</span></a>
             </li>
 
             <!-- Divider -->
