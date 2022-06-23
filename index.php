9d9c6d08c5adcfdcc701b02f91f8d2fbcadde61c
Your Name <you@example.com>
2022-06-23 14:13:36 -0500

    V1.0.1

--- index.php
+++ index.php
@@ -69,12 +69,12 @@
                 <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
                     aria-expanded="true" aria-controls="collapseTwo">
                     <i class="fas fa-fw fa-cog"></i>
-                    <span>Components</span>
+                    <span>Componentes</span>
                 </a>
                 <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
                     <div class="bg-white py-2 collapse-inner rounded">
                         <h6 class="collapse-header">Custom Components:</h6>
-                        <a class="collapse-item" href="buttons.html">Buttons</a>
+                        <a class="collapse-item" href="buttons.html">Botones</a>
                         <a class="collapse-item" href="cards.html">Cards</a>
                     </div>
                 </div>
